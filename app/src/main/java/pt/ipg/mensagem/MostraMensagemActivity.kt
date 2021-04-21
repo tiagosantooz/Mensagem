1cfe559138164d41216df64975ef04038a21872f
tiagosantooz <tiago_slash@hotmail.com>
2021-04-21 10:56:50 +0100

    Mostrar a mensagem na segunda activity

--- app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
@@ -2,10 +2,15 @@ package pt.ipg.mensagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostraMensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem)
+        val mensagem =  intent.getStringExtra("MENSAGEM")
+        val textviewMensagem = findViewById<TextView>(R.id.textView2)
+        textViewMensagem.setText(mensagem)
+
     }
 }
