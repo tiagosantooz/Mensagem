f4252d3cb580168bdf6170c9af720cdd25def2e0
tiagosantooz <tiago_slash@hotmail.com>
2021-04-21 10:48:41 +0100

    Criaação de um intent para passar de uma atividade para outra

--- app/src/main/java/pt/ipg/mensagem/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.kt
@@ -1,8 +1,10 @@
 package pt.ipg.mensagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun enviamensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+        startActivity(intent)
+
 
     }
 }
